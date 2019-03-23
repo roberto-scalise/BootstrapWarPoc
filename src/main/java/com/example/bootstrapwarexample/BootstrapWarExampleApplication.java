88779e872d5fa911cf80d7dc94e8eb226eedeaca
roberto-scalise <48862116+roberto-scalise@users.noreply.github.com>
2019-03-23 16:45:13 +0100

    first commit

---
+++ src/main/java/com/example/bootstrapwarexample/BootstrapWarExampleApplication.java
@@ -0,0 +1,25 @@
+package com.example.bootstrapwarexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BootstrapWarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapWarExampleApplication.class, args);
+	}
+	
+	@RestController
+	class GreetingController {
+	    
+	    @RequestMapping("/hello/{name}")
+	    String hello(@PathVariable String name) {
+	        return "Hello, " + name + "!";
+	    }
+	}
+
+}
